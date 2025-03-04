98dcdf08507567fb402698a92072cb1a2b3d417f
waengs <144982285+waengs@users.noreply.github.com>
2025-03-04 19:50:34 +0700

    ded3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import { TodoWrapper } from './components/TodoWrapper'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing } from "./Landing";
+import { Profile } from "./Profile";
+import { TodoWrapper } from "./TodoWrapper";
+import { NavBar } from "./NavBar"; // Import NavBar
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-            <Router>
-                <Routes>
-                    <Route path='/' element={<TodoWrapper />}></Route>
-                </Routes>
-            </Router>
-        </div>
-  )
+    <Router>
+      <NavBar /> {/* Add the navigation bar */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/todo" element={<TodoWrapper />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
